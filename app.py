29a8a2389c12da1d488a2e852e16ce39db389668
Ephraim Becker <emb@paay.co>
2022-06-08 14:36:16 -0400

    Trying to connect mysql.

--- app.py
+++ app.py
@@ -1,25 +1,23 @@
 from flask import Flask, render_template
-import mysql.connector
+from flaskext.mysql import MySQL
 import datetime
+# import config
 
 app = Flask(__name__)
 
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'paayco'
+app.config['MYSQL_DATABASE_DB'] = 'products'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql = MySQL(app)
+
 @app.route('/')
 def hello_world():
-    mydb = mysql.connector.connect(
-        host="localhost",
-        user="paay",
-        password="123",
-        database="products"
-    )
+    cursor = mysql.get_db().cursor()
 
-    # mycursor = mydb.cursor()
+    cursor.execute("SELECT * from products")
 
-    # x = datetime.datetime.now()
+    rows = cursor.fetchall()
 
-    # sql = "INSERT INTO products (ProductTitle, ProductDescription, DateCreated, DateModified) VALUES (%s, %s, %s, %s)"
-    # val = ("JavaScript API key", "JavaScript API key for 3D secure.", x, x)
-    # mycursor.execute(sql, val)
-
-    mydb.commit()
-    return render_template('index.html')
+    return render_template('index.html', products = rows)

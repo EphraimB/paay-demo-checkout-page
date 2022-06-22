3e5a62d8a3215f13162031fe8c8ed0d3ba01168b
Ephraim Becker <emb@paay.co>
2022-06-22 15:27:57 -0400

    Made the login a popup.

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,4 @@
+var loginPopup = document.getElementById("loginPopup");
 function highlightNavItem() {
     var links = document.getElementById("divWheel");
   
@@ -20,8 +21,36 @@ wheel.slicePathCustom.minRadiusPercent = 0.3;
 wheel.slicePathCustom.maxRadiusPercent = 0.6;
 wheel.sliceSelectedPathCustom = wheel.slicePathCustom;
 wheel.sliceInitPathCustom = wheel.slicePathCustom;
+
+wheel.spreaderEnable = true;
+wheel.spreaderInTitle = "imgsrc:/static/icons/login.svg"
+wheel.spreaderOutTitle = "imgsrc:/static/icons/login.svg"
+wheel.spreaderInPercent = 1.0;
+wheel.spreaderOutPercent = 1.0;
+wheel.spreaderRadius = wheel.wheelRadius * 0.25;
+wheel.spreaderPathInAttr = { fill: '#FFF' };
+wheel.spreaderPathOutAttr = { fill: '#FFF' }
+
 wheel.createWheel();
 wheel.navigateWheel(highlighttedNav);
-wheel.setTooltips(["Home", "Cart", "Login"]);
+wheel.setTooltips(["Home", "Cart"]);
 wheel.sliceSelectedAttr = { stroke: '#111111', 'stroke-width': 4 };
 wheel.refreshWheel()
+
+var wheelspreader = document.getElementById("wheelnav-divWheel-spreader");
+var wheelspreadertitle = document.getElementById("wheelnav-divWheel-spreadertitle");
+
+wheelspreader.onmouseup = function() {
+  loginPopup.classList.remove("d-none");
+  loginPopup.classList.add("d-flex", "position-absolute", "top-50", "start-50", "translate-middle", "card", "text-bg-light");
+  openSpreader();
+}
+wheelspreadertitle.onmouseup = function() {
+  loginPopup.classList.remove("d-none");
+  loginPopup.classList.add("d-flex", "position-absolute", "top-50", "start-50", "translate-middle", "card", "text-bg-light");
+  openSpreader();
+}
+
+var openSpreader = function() {
+  wheel.minPercent = wheel.maxPercent;
+}

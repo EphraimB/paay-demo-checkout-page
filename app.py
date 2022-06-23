691f0d37f608f4a6433e39ab0442108e5c4dfd4e
Ephraim Becker <emb@paay.co>
2022-06-23 12:27:17 -0400

    Log in and out correctly.

--- app.py
+++ app.py
@@ -1,23 +1,70 @@
-from flask import Flask, render_template
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from passlib.hash import sha256_crypt
 import datetime
 import config
+import os
+import operator
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    cursor = config.db.cursor()
+    cursor = config.dbPAAY.cursor()
 
-    cursor.execute("SELECT * from products")
+    cursor.execute("SELECT * FROM products")
 
     rows = cursor.fetchall()
 
-    return render_template('index.html', products = rows)
+    return render_template('index.html', products = rows, loggedIn = session.get('logged_in'))
 
-@app.route('/admin-login/')
+@app.route('/login', methods=['POST'])
 def login():
-    return render_template('/admin-login/index.html')
+    login = request.form
+
+    username = login['username']
+    password = login['password']
+
+    cursor = config.dbUsers.cursor(buffered=True)
+
+    data = cursor.execute('SELECT * FROM Users WHERE username=%s', (username,))
+    data = cursor.fetchone()[2]
+
+    if sha256_crypt.verify(password, data):
+        account = True
+    else:
+        account = False
+
+    if account == True:
+        session['logged_in'] = True
+    else:
+        session['logged_in'] = False
+    return index()
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    return
+    signup = request.form
+
+    username = signup['username']
+    password = sha256_crypt.encrypt(signup['password'])
+    email = signup['email']
+
+    cursor = config.dbUsers.cursor(buffered=True)
+
+    cursor.execute('INSERT INTO Users (username, password, email) VALUES (%s, %s, %s)', (username, password, email))
+
+    config.dbUsers.commit()
+
+    cursor.close()
+
+    return index()
+
+@app.route('/logout/')
+def logout():
+  session['logged_in'] = False
+  return index()
+
+if __name__ == "__main__":
+  app.run(debug=True,host='0.0.0.0', port=5000)
+
+if __name__ == "app":
+    app.secret_key = os.urandom(12)
